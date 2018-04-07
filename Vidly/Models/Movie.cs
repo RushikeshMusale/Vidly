267e8f221e62ee44c59719e3ff6e33b2d02f3d29
RushikeshMusale <rushikeshmusale@gmail.com>
2018-04-08 00:24:25 +0530

    Made Name, Field Required

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,19 +9,23 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
 
         [Display(Name ="Release Date")]
+        [Required]
         public DateTime ReleaseDate { get; set; }
         public DateTime DateAdded { get; set; }
 
         [Display(Name ="Number In Stock")]
+        [Required]
         public int NumberInStock { get; set; }
 
         //Navigation Properties
         public Genre Genre { get; set; }
 
         [Display(Name ="Genre")]
+        [Required]
         public byte GenreId { get; set; }
     }
 }
