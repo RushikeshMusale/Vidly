83b1ce53f29d143c8d979f278a44a14e8c19a637
RushikeshMusale <rushikeshmusale@gmail.com>
2018-04-08 09:52:54 +0530

    Handling of required field validation

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -74,6 +74,16 @@ namespace Vidly.Controllers
         // NOTE: Parameter name should be customer here
         public ActionResult Save(Customer customer)
         {
+            if(!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm",viewModel);
+            }
+
             if(customer.Id==0)            
                 _context.Customers.Add(customer);
             else
